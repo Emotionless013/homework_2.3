65b9c655a3619b2208f5f09cd2120034d4f02962
Alexander <emotionless013@mail.ru>
2022-08-18 22:04:29 +0400

    hw

---
+++ src/Car.java
@@ -0,0 +1,26 @@
+public class Car {
+
+    private String modelName;
+    private int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public Car(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+}
